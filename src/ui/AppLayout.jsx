f1e3a6dffa5bd82df3e1a39e04e9974491a19970
Rex <lubwamaemmanuel1@gmail.com>
2023-09-13 18:01:39 +0300

    feat:(ui components)

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,14 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 export default function AppLayout() {
-    return (
-        <div>
-            <Header />
-            
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
 
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,9 @@
 export default function Header() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="hover:drop-shadow-[0_0_2em_rgba(100, 108, 255, 0.667)] w-20 bg-red-700 text-3xl">
+        <h2>rex</h2>
+      </div>
+    </div>
+  );
 }
-
