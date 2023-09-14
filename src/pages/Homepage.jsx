ad87908b593144da22226ded2a920d5b087e943f
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-14 16:42:23 +0300

    feat:(added all pages)

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Homepage() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
---
+++ src/pages/Services.jsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <div>
+      <h3>services</h3>
+    </div>
+  );
+}
