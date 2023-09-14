b33c32b2c3fd88f571ed59c00864696520efc16a
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-14 16:43:54 +0300

    refactor:(clean up)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import AppLayout from "./ui/AppLayout"
-import Homepage from "./pages/Homepage"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
 
 export default function App() {
-
-
-  const router = createBrowserRouter([{
-    element: <AppLayout />,
-    children: [{
+  const router = createBrowserRouter([
+    {
       path: "/",
-      element: <Homepage />
-    }]
-  }])
+      element: <Homepage />,
+    },
+    {
+      path: "/products",
+      element: <Products />,
+    },
+    {
+      path: "/services",
+      element: <Services />,
+    },
+    {
+      path: "/contact",
+      element: <Contact />,
+    },
+  ]);
 
-  return <RouterProvider router={router} />
-  
+  return <RouterProvider router={router} />;
 }
-
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,17 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="flex items-center justify-between gap-[20%] px-5 py-2">
       <div className="text-center">
-        <h2 className="text-3xl font-semibold text-red-500">shop with rex</h2>
-        <p className="text-sm italic">the tech guy</p>
+        <h2 className=" text-3xl font-semibold ">shop with rex</h2>
+        <p className=" text-sm italic">the tech guy</p>
       </div>
       <ul className="flex flex-grow justify-between text-xl">
-        <li>products</li>
-        <li>services</li>
-        <li>contact</li>
+        <li>
+          <NavLink to="/products">products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">services</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">contact</NavLink>
+        </li>
       </ul>
       <div>
-        <button className="bg-red-500 px-2 py-1 text-xl font-bold">shop</button>
+        <button className=" px-2 py-1 text-xl font-bold">shop</button>
       </div>
     </header>
   );
