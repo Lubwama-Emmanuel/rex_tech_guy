554dbeb6b7779a1a7714d4f7fef81b281c54cc85
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-14 16:43:20 +0300

    feat:(added more components)

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import Header from "../ui/Header";
+
+export default function Hero() {
+  return (
+    <div className="h-[90vh] bg-gradient-to-r from-emerald-400 to-emerald-700">
+      <Header />
+    </div>
+  );
+}
