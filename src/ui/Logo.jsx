dfdb2cbf19970ba2941265af77b0fcb23aebf003
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-16 23:01:44 +0300

    feat:(moved logo to reusable component)

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <h2 className=" font-logo bg-gradient-to-r from-emerald-600 to-emerald-800 bg-clip-text text-3xl font-semibold text-transparent ">
+      shop with rex
+    </h2>
+  );
+}
