3fc4773679ca7c7c52a46e6242ad1211e98d68d3
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-18 17:35:52 +0300

    feat:(changed links to an object)

--- src/ui/Links.jsx
+++ src/ui/Links.jsx
@@ -1,23 +1,33 @@
 import { NavLink } from "react-router-dom";
 
+const linkItems = [
+  {
+    id: 0,
+    item: "products",
+    link: "/products",
+  },
+  {
+    id: 0,
+    item: "services",
+    link: "/services",
+  },
+  {
+    id: 0,
+    item: "contact",
+    link: "/contact",
+  },
+];
+
 export default function Links() {
   return (
     <ul className="flex justify-between space-x-12 text-xl uppercase">
-      <li>
-        <NavLink to="/products" className="focus:text-emerald-500">
-          products
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/services" className="focus:text-emerald-500">
-          services
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/contact" className="focus:text-emerald-500">
-          contact
-        </NavLink>
-      </li>
+      {linkItems.map((el) => (
+        <li key={el.id}>
+          <NavLink to="/products" className="focus:text-emerald-500">
+            products
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 }
