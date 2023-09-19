6976387ffa9acb2699ab5a98e0a723d76780cf92
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-19 21:48:15 +0300

    feat:(added product overview component)

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -1,3 +1,23 @@
 export default function ProductOverview() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="w-96">
+        <img src="/images/omen-16.png" className="w-96" />
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(140px,_1fr))]">
+          <div>
+            <img src="/images/omen-22.png" className="w-32" />
+          </div>
+          <div>
+            <img src="/images/omen-intel.png" className="w-32" />
+          </div>
+          <div>
+            <img src="/images/omen-transcend.png" className="w-32" />
+          </div>
+          <div>
+            <img src="/images/omen-16.png" className="w-32" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
