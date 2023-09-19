4b1e276aa87b4ec1bc2d72ac6f63ee9308013cc8
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-19 21:56:41 +0300

    feat:(added product overview component)

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <h2 className=" bg-gradient-to-r from-emerald-600 to-emerald-800 bg-clip-text font-logo text-3xl font-semibold text-transparent ">
+    <h2 className="bg-gradient-to-r from-emerald-600 to-emerald-800 bg-clip-text font-logo text-3xl font-semibold text-transparent ">
       <NavLink to="/">shop with rex</NavLink>
     </h2>
   );
