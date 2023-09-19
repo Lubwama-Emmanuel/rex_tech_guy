8e6eb756ba7de96097a672dd991a052ff8250c6c
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-19 21:24:40 +0300

    feat:(added product page to pages)

--- src/ui/ProductCard.jsx
+++ src/ui/ProductCard.jsx
@@ -1,8 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 export default function ProductCard({ laptop }) {
   const { name, image } = laptop;
+
   return (
-    <div className="flex flex-col items-center hover:scale-110">
+    <div
+      className="flex flex-col items-center hover:scale-110"
+      onClick={() => <NavLink to="/products/gaming/omen" />}
+    >
       <img src={image} className="w-64" />
       <h3 className="uppercase">{name}</h3>
       <h3>Ugx 3,000,000</h3>
