77157e5ca20876b91eea81fe5c9bdb01b3b9709e
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-21 11:41:49 +0300

    refactor:(refactored code structured)

--- src/ui/ProductCard.jsx
+++ src/ui/ProductCard.jsx
@@ -8,7 +8,7 @@ export default function ProductCard({ laptop }) {
       onClick={() => console.log("clicked")}
     >
       <img src={image} className="w-64" />
-      <h3 className="uppercase">{name}</h3>
+      <h3 className="text-md text-center uppercase">{name}</h3>
       <h3>Ugx 3,000,000</h3>
     </div>
   );
