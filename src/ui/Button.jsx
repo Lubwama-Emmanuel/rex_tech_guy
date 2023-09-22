d03ecd6c080b684f2e7096032c6198962fda7825
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-22 14:53:06 +0300

    feat:(added box shawdow on buttons)

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
-export default function Button({ children }) {
+export default function Button({ children, handleClick }) {
   return (
-    <button className=" rounded-full border border-emerald-400 px-4 pb-1 pt-2 text-xl font-bold uppercase leading-none text-emerald-800">
+    <button
+      onClick={() => handleClick()}
+      className="shawdow-emerald-500/50 rounded-full border border-emerald-400 px-4 pb-1 pt-2 font-bold uppercase leading-none text-emerald-800 shadow-lg sm:text-xl"
+    >
       {children}
     </button>
   );
