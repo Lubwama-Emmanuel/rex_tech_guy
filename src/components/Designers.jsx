cc27abcf6ef3ba27f9a552b481795340486394ee
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-23 22:13:05 +0300

    feat:(added designers catergory)

---
+++ src/components/Designers.jsx
@@ -0,0 +1,43 @@
+import ComponentHead from "../ui/ComponentHead";
+import ProductCard from "../ui/ProductCard";
+
+const data = [
+  {
+    id: 0,
+    name: "hp omen 16 (2022 amd)",
+    image: "/images/omen-22.png",
+  },
+  {
+    id: 1,
+    name: "victus 16 gaming laptop (2023 intel)",
+    image: "/images/victus-16.png",
+  },
+  {
+    id: 2,
+    name: "omen transcend 16 gaming laptop",
+    image: "/images/omen-transcend.png",
+  },
+  {
+    id: 3,
+    name: "omen 16 gaming laptop (2023 intel",
+    image: "/images/omen-intel.png",
+  },
+  {
+    id: 4,
+    name: "omen 16 gaming laptop (2022 amd)",
+    image: "/images/omen-16.png",
+  },
+];
+
+export default function Designers() {
+  return (
+    <section className="m-auto mb-20 w-[95%]">
+      <ComponentHead title={"designers"} link={"/products/designers"} />
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))]">
+        {data.map((el) => (
+          <ProductCard key={el.id} laptop={el} />
+        ))}
+      </div>
+    </section>
+  );
+}
