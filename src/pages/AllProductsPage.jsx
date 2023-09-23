5e55974d62503a5f8380283989b2fa7628174e21
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-23 23:18:22 +0300

    refactor:(making the app functionlity better)

--- src/pages/AllProductsPage.jsx
+++ src/pages/AllProductsPage.jsx
@@ -1,28 +1,63 @@
+/* eslint-disable react/prop-types */
 import Hero from "../components/Hero";
 import FilterButton from "../ui/FilterButton";
 import Products from "../components/Products";
+import { useEffect, useState } from "react";
 
 export default function AllProductsPage() {
+  const [isFilterToTop, setIsFilterToTop] = useState(false);
+  useEffect(() => {
+    // Function to be handling scrolling event
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+
+      console.log(scrollY);
+
+      if (scrollY === 400) {
+        console.log("reached");
+        setIsFilterToTop(true);
+      } else if (scrollY < 400) {
+        setIsFilterToTop(false);
+      }
+    };
+
+    // Adding acroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleaning up th event lsitener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <section>
       <Hero />
-      <ProductsSection />
+      <h2 className="mb-5 text-center text-5xl text-emerald-600">
+        Which laptop is right for you?
+      </h2>
+      <ProductsSection isFilterToTop={isFilterToTop} />
     </section>
   );
 }
 
-function ProductsSection() {
+function ProductsSection({ isFilterToTop }) {
   return (
     <section className="m-auto max-w-[98%]">
-      <Filters />
+      <Filters isFilterToTop={isFilterToTop} />
       <ProductsList />
     </section>
   );
 }
 
-function Filters() {
+function Filters({ isFilterToTop }) {
+  const base = `flex gap-2 sm:gap-5 overflow-x-auto justify-between py-7 bg-white`;
   return (
-    <div className="flex justify-between">
+    <div
+      className={
+        isFilterToTop ? `${base} fixed left-0 right-0 top-14 z-20` : base
+      }
+    >
       <FilterButton isActive={true}>All products</FilterButton>
       <FilterButton>Gaming</FilterButton>
       <FilterButton>Designing</FilterButton>
