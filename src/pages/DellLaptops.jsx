753be8c6722c5a9f7747f051c8d75b46b6abe6f0
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-25 23:18:20 +0300

    feat:(added dell laptops page)

---
+++ src/pages/DellLaptops.jsx
@@ -0,0 +1,58 @@
+import Hero from "../components/Hero";
+import ProductsSection from "../ui/ProductsSection";
+
+const DellFilters = [
+  {
+    id: 0,
+    filter: "Dell",
+  },
+  {
+    id: 1,
+    filter: "xps",
+  },
+  {
+    id: 2,
+    filter: "precision",
+  },
+  {
+    id: 3,
+    filter: "latitude",
+  },
+  {
+    id: 4,
+    filter: "inspiron",
+  },
+  {
+    id: 5,
+    filter: "2 in 1",
+  },
+  {
+    id: 6,
+    filter: "touch screen",
+  },
+  {
+    id: 7,
+    filter: "gaming",
+  },
+  {
+    id: 8,
+    filter: "gpu",
+  },
+  {
+    id: 9,
+    filter: "11th gen",
+  },
+  {
+    id: 10,
+    filter: "12th gen",
+  },
+];
+
+export default function DellLaptops() {
+  return (
+    <div>
+      <Hero />
+      <ProductsSection filters={DellFilters} />
+    </div>
+  );
+}
