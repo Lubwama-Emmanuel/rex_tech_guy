0cbcf494d0132c973c6c4ce809824be544ca2709
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-25 21:15:45 +0300

    feat:(made product card responsive)

--- src/ui/ProductCard.jsx
+++ src/ui/ProductCard.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/prop-types */
-export default function ProductCard({ laptop }) {
+export default function ProductCard({ laptop, larger = false }) {
   const { name, image } = laptop;
+
+  //
 
   return (
     <div
-      className="flex flex-col items-center sm:hover:scale-110"
+      className={
+        larger
+          ? `flex items-center sm:hover:scale-110`
+          : `flex flex-col items-center sm:hover:scale-110`
+      }
       onClick={() => console.log("clicked")}
     >
       <img src={image} className="w-64" />
-      <h3 className="sm:text-md text-center text-sm uppercase">{name}</h3>
-      <h3 className="text-sm">Ugx 3,000,000</h3>
+      <div>
+        <h3 className="sm:text-md text-center text-sm uppercase">{name}</h3>
+        <h3 className="text-sm">Ugx 3,000,000</h3>
+      </div>
     </div>
   );
 }
