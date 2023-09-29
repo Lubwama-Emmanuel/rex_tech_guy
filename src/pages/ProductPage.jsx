e56b4be3c5fffaecf605b7a4534db9f3a1f9f565
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-09-29 21:59:01 +0300

    feat:(made component able to recieve dynamic laptop)

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,15 @@
+import { useLoaderData } from "react-router-dom";
 import ProductOverview from "../components/ProductOverview";
 import Products from "../components/Products";
 
 export default function ProductPage() {
+  const data = useLoaderData();
+
+  console.log(data);
+
   return (
     <div className="m-auto w-[95%]">
-      <ProductOverview />
+      <ProductOverview laptop={data} />
       <Products />
     </div>
   );
