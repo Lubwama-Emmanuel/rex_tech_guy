933c21cff9d61291af66fdc9f22cd335de1e54dd
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-24 19:50:13 +0300

    feat:changed hovers to scaling

--- src/ui/ProductCard.jsx
+++ src/ui/ProductCard.jsx
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router-dom";
+import Button from "./Button";
 
 /* eslint-disable react/prop-types */
 export default function ProductCard({ laptop, larger = false }) {
@@ -6,21 +7,23 @@ export default function ProductCard({ laptop, larger = false }) {
 
   return (
     <div
-      // className="grid grid-cols-[repeat(auto-fit_min-max(100px_1fr))] grid-rows-[3fr_1fr] gap-cols-2"
       className={
         larger
-          ? `flex items-center sm:hover:scale-110`
-          : `flex flex-col items-center sm:hover:scale-110`
+          ? `flex items-center border-emerald-900 sm:hover:border`
+          : `flex flex-col items-center rounded-xl border-emerald-400 p-5 sm:hover:border`
       }
       onClick={() => <Navigate to={`/products/${id}`} />}
     >
       <div className="flex items-center justify-center">
         <img src={mainImage} className="w-32 sm:w-64" />
       </div>
-      <div className="">
+      <div className="space-y-2 text-center">
         <h3 className="sm:text-md text-center text-sm uppercase">{name}</h3>
         <h3 className="text-sm">Ugx 3,000,000</h3>
-        {/* <Link to={`/products/${id}`}>To</Link> */}
+        <div className="flex justify-center gap-5 text-center">
+          <Button>{"shop"}</Button>
+          <Button>{"Checkout"}</Button>
+        </div>
       </div>
     </div>
   );
