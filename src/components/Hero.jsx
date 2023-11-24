101009b41cd4adbe5f5f9f5d110ef3fa18cfdab8
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-24 19:48:11 +0300

    feat:changed height for the con to auto

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import Slider from "./Slider";
 
 export default function Hero() {
   return (
-    <section className="h-[500px] sm:h-[95dvh]">
+    <section className="flex h-[500px] items-end justify-center  sm:h-auto">
       <Slider />
     </section>
   );
