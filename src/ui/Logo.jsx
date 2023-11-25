1a36e1d707848619a7dbec0ae71838cf871bf74f
Lubwama Emmanuel <lubwamaemmanuel1@gmail.com>
2023-11-25 21:23:31 +0300

    feat:modified the logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 
+// Company Logo component
 export default function Logo() {
   return (
     <h2 className="bg-gradient-to-r from-emerald-500 to-emerald-800 bg-clip-text font-logo text-2xl font-semibold text-transparent sm:text-3xl ">
